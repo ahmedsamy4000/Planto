0df1108637bac643ad712435a36947b474ac318c
ahmed <https://github.com/ahmedsamy4000>
2024-03-25 21:02:00 +0200

    samyy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { OurStoryComponent } from './Components/our-story/our-story.component';
 import { CartItemsComponent } from './Components/cart-items/cart-items.component';
 import { CartEmptyComponent } from './Components/cart-empty/cart-empty.component';
 import { ProductComponent } from './Components/Shop/product/product.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 export const routes: Routes = [
     {path: "shop", component: ProductsComponent},
     {path: "ourStory", component: OurStoryComponent},
     {path: "cartItems", component: CartItemsComponent},
     {path: "cartEmpty", component: CartEmptyComponent},
-    {path: "shop/:name", component: ProductComponent}
+    {path: "shop/:name", component: ProductComponent},
+    {path: "contact",component:ContactComponent}
 ];
