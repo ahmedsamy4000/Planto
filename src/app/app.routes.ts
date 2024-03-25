4be04023bb433777014c7442f87334bcbf8f0b83
merafahmishokri <mera.fahmy219@gmail.com>
2024-03-25 04:13:08 +0200

    Mera

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './Components/Shop/products/products.component';
+import { OurStoryComponent } from './Components/our-story/our-story.component';
+import { CartItemsComponent } from './Components/cart-items/cart-items.component';
+import { CartEmptyComponent } from './Components/cart-empty/cart-empty.component';
 
 export const routes: Routes = [
-    {path: "shop", component: ProductsComponent}
+    {path: "shop", component: ProductsComponent},
+    {path: "ourStory", component: OurStoryComponent},
+    {path: "cartItems", component: CartItemsComponent},
+    {path: "cartEmpty", component: CartEmptyComponent}
+
+
+
 ];
