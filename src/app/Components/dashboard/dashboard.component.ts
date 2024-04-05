b40b0bdce5bc5174df1bfb0e2ddd97f7eb1b9c63
Paula Nabil <pola.nabil.206@gmail.com>
2024-04-05 03:07:30 +0200

    pola

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
-import { Component,OnInit } from '@angular/core';
-import {Chart,ChartOptions} from 'chart.js/auto';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartOptions } from 'chart.js/auto';
 import { ReceiptService } from '../../Services/recieptService';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -7,82 +7,74 @@ import { HttpClientModule } from '@angular/common/http';
   selector: 'app-dashboard',
   standalone: true,
   imports: [HttpClientModule],
-  providers:[ReceiptService],
+  providers: [ReceiptService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  constructor(private http:ReceiptService){}
- monthSelectedValue="";
- bestsellingobj:any;
+export class DashboardComponent implements OnInit {
+  monthSelectedValue = "4";
+  bestsellingobj: any;
+  productsNames: any;
+  productvalues: any;
+  myChart: Chart | undefined;
+
+  constructor(private http: ReceiptService) { }
+
   onLanguageSelect(event: any): void {
-    this.monthSelectedValue=event.target.value;
+    this.monthSelectedValue = event.target.value;
     console.log(this.monthSelectedValue);
+    this.fetchData();
+  }
+  ngOnInit() {
+    this.fetchData();
+  }
+  fetchData() {
     this.http.getRecieptsByMonth(this.monthSelectedValue).subscribe({
-      next:(data)=>{
-        this.bestsellingobj=data;
+      next: (data) => {
+        this.bestsellingobj = data;
         console.log(this.bestsellingobj);
+        this.productsNames = Object.keys(this.bestsellingobj.data);
+        this.productvalues = Object.values(this.bestsellingobj.data);
+        this.updateChart();
       },
-      error:(error)=>{
+      error: (error) => {
         console.log(error);
       }
     });
   }
 
-  ngOnInit() {
-    
-    // Data for the chart
-    const xValues: string[] = ["Product1", "Product2", "Product3", "Product4", "Product5"];
-    const yValues: number[] = [80, 49, 44, 24, 15];
-    const barColors: string[] = ["red", "green","blue","orange","brown"];
+  updateChart() {
+    if (this.myChart) {
+      this.myChart.data.labels = this.productsNames;
+      this.myChart.data.datasets[0].data = this.productvalues;
+      this.myChart.update();
+    } else {
+      const barColors: string[] = ["red", "green", "blue", "orange", "brown"];
 
-    // Chart options
-    const options: ChartOptions<'bar'> = {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      },
-      plugins: {
-        legend: {
-          display: false
-        }
-      },
-    };
+      const options: ChartOptions<'bar'> = {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+      };
 
-    // Create a new chart instance
-    const myChart = new Chart("myChart", {
-      type: "bar",
-      data: {
-        labels: xValues,
-        datasets: [{
-          backgroundColor: barColors,
-          data: yValues
-        }]
-      },
-      options: options
-    });
+      this.myChart = new Chart("myChart", {
+        type: "bar",
+        data: {
+          labels: this.productsNames,
+          datasets: [{
+            backgroundColor: barColors,
+            data: this.productvalues
+          }]
+        },
+        options: options
+      });
+    }
   }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
