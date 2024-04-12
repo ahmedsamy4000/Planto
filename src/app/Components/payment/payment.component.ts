90fb4f5cc17b3bbc6aa61c721985f36940ad41ba
reem <hatemreem37@gmail.com>
2024-04-13 00:14:08 +0200

    reem

--- src/app/Components/payment/payment.component.ts
+++ src/app/Components/payment/payment.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../Services/user.service';
 import { PaymentitemComponent } from './paymentitem/paymentitem.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [PaymentitemComponent,HttpClientModule],
+  imports: [PaymentitemComponent, HttpClientModule, RouterModule, CommonModule],
   providers:[UserService],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
@@ -14,9 +16,20 @@ import { HttpClientModule } from '@angular/common/http';
 export class PaymentComponent implements OnInit {
 items:any;
 numOfItems=0;
+totalPrice=0;
+userData: any;
 
-  constructor(private user:UserService){}
+  constructor(private user:UserService, private router:Router){}
   ngOnInit(): void {
+    this.user.GetUserByEmail(localStorage.getItem("Email")).subscribe({
+      next:(data)=>{
+        this.userData=data;
+        this.userData=this.userData.data;
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    });
     this.user.GetCart(localStorage.getItem("Email")).subscribe({
       next:(data)=>{
         this.items=data;
@@ -25,11 +38,19 @@ numOfItems=0;
         for(let item of this.items){
           console.log(item)
           this.numOfItems+=+item.quantity;
+          this.totalPrice+=item.quantity*item.product.price;
         }
       },
       error:(err)=>{
         console.log(err)
       }
     });
+  }
+  
+  // goToShop(){
+  //   this.router.navigate(['/shop']);
+  // }
+  output(data:number){
+    this.totalPrice+=+data;
   }
 }
