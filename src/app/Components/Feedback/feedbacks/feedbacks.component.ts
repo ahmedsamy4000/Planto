ca585f8cd8e4659854537d9f9839833260935d03
reem <hatemreem37@gmail.com>
2024-04-15 18:33:06 +0200

    feedback

--- src/app/Components/Feedback/feedbacks/feedbacks.component.ts
+++ src/app/Components/Feedback/feedbacks/feedbacks.component.ts
@@ -3,29 +3,41 @@ import { FeedBackService } from '../../../Services/feedbackService';
 import { HttpClientModule } from '@angular/common/http';
 import { FeedbackEmptyComponent } from '../feedback-empty/feedback-empty.component';
 import { FeedbackItemComponent } from '../feedback-item/feedback-item.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-feedbacks',
   standalone: true,
-  imports: [FeedbackEmptyComponent,FeedbackItemComponent,HttpClientModule,CommonModule],
-  providers:[FeedBackService],
+  imports: [FeedbackEmptyComponent, FeedbackItemComponent, HttpClientModule, CommonModule],
+  providers: [FeedBackService],
   templateUrl: './feedbacks.component.html',
   styleUrl: './feedbacks.component.css'
 })
-export class FeedbacksComponent implements OnInit{
-  feedbacks:any;
-  constructor(private feedbackhttp:FeedBackService){}
+export class FeedbacksComponent implements OnInit {
+  feedbacks: any;
+  formattedDate: string = '';
+  constructor(private feedbackhttp: FeedBackService) { }
   ngOnInit(): void {
     this.feedbackhttp.getFeedBacks().subscribe({
-      next:(data)=>{
-        this.feedbacks=data;
-        this.feedbacks=this.feedbacks.data;
+      next: (data) => {
+        this.feedbacks = data;
+        this.feedbacks = this.feedbacks.data;
+
+        for (var i of this.feedbacks) {
+          const date = new Date(i.date);
+
+          // Format the date using Angular's DatePipe
+          // this.formattedDate = this.datePipe.transform(date, 'medium') || '';
+          this.formattedDate = i.date.substring(0, 10);
+
+          i.date = this.formattedDate;
+
+        }
       },
-      error:(error)=>{
+      error: (error) => {
         console.log(error);
       }
     })
   }
-  
+
 }
